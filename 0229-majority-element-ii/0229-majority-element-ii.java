88b3b872f57a9b7e5d536473ca11c7f28a50fa27
Aniket775 <78209263+Aniket775@users.noreply.github.com>
2023-11-01 01:13:24 +0530

    Time: 3 ms (70.01%), Space: 46.8 MB (34.08%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,41 +1,42 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> list=new ArrayList<Integer>();
-        Arrays.sort(nums);
-        if(nums.length<2)
+
+        int cnt1=0;
+        int cnt2=0;
+        int e1=Integer.MIN_VALUE;;
+        int e2=Integer.MIN_VALUE;;
+
+        for(int i=0;i<nums.length;i++)
         {
-            list.add(nums[0]);
-            return list;
-        }
-        if(nums.length<3)
-        {
-            if(nums[0]==nums[1])
+            if(cnt1==0 && nums[i]!=e2)
             {
-                list.add(nums[0]);
+                cnt1=1;
+                e1=nums[i];
             }
-            else
+            else if(cnt2==0 && nums[i]!=e1)
             {
-                list.add(nums[0]);
-                list.add(nums[1]);
+                cnt2=1;
+                e2=nums[i];
             }
-            return list;
-        }
-        int c=1;
-        for(int i=1;i<nums.length;i++)
-        {
-            if(nums[i-1]==nums[i])
+            else if(nums[i]==e1) cnt1++;
+            else if(nums[i]==e2) cnt2++;
+            else 
             {
-                c++;
-                if(c>nums.length/3)
-                {
-                    if(c<=(nums.length/3)+1) list.add(nums[i]);
-                }
-            }
-            else
-            {
-                c=1;
+                cnt1--;
+                cnt2--;
             }
         }
+
+        cnt1 = 0; cnt2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == e1) cnt1++;
+            if (nums[i] == e2) cnt2++;
+        }
+
+        //int mini = (int)(nums.length / 3) + 1;
+        if (cnt1 >= (nums.length / 3) + 1) list.add(e1);
+        if (cnt2 >= (nums.length / 3) + 1) list.add(e2);
         return list;
     }
 }
