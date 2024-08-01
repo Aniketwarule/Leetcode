c06f3fe882bcab9183638baeb5af7f6e10a2c961
Aniket Warule <78209263+Aniketwarule@users.noreply.github.com>
2024-08-01 09:37:44 +0530

    Time: 0 ms (100%), Space: 42.2 MB (84.64%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
@@ -4,8 +4,8 @@ class Solution {
         for(int i=0;i<details.length;i++)
         {
             String s = "";
-            int a = Integer.parseInt(details[i].charAt(11)+"");
-            int b = (a*10)+Integer.parseInt(details[i].charAt(12)+"");
+            int a = details[i].charAt(11)-'0';
+            int b = (a*10)+(details[i].charAt(12)-'0');
             
             if(b>60) c++;
         }
