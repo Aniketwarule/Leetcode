853fe95181988204de0856015059445d4d35f4e5
Aniket Warule <78209263+Aniketwarule@users.noreply.github.com>
2025-02-27 19:09:53 +0530

    Time: 10 ms (11.74%), Space: 40.2 MB (78.96%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,12 +1,7 @@
 class Solution {
     public int fib(int n) {
         if(n==0) return 0;
-        if(n==1 || n==2) return 1;
-        return fi(n);
-    }
-    public int fi(int n)
-    {
-        if(n==2||n==1) return 1;
-        return fi(n-1)+fi(n-2);
+        if(n==1) return 1;
+        return fib(n-1)+fib(n-2);
     }
 }
