0b78197d6d600432cff64dada6d0f1b3fed6b666
Aniket Warule <78209263+Aniketwarule@users.noreply.github.com>
2025-08-01 23:40:47 +0530

    Time: 2 ms (82.75%), Space: 45.1 MB (12.92%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -2,21 +2,22 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
 
-        makeCombination(candidates, target, 0, new ArrayList<>(), 0, res);
+        back(candidates, target, 0, res, new ArrayList<>());
         return res;
+    }  
+    public void back(int[] arr,int tar,int idx, List<List<Integer>> res, List<Integer> ds)
+    {
+        if(idx==arr.length)
+        {
+            if(tar==0) res.add(new ArrayList<>(ds));
+            return;
+        }
+        if(arr[idx]<=tar)
+        {
+            ds.add(arr[idx]);
+            back(arr, tar-arr[idx], idx, res, ds);
+            ds.remove(ds.size()-1);
+        }
+        back(arr, tar, idx+1, res, ds);
     }
-
-    private void makeCombination(int[] candidates, int target, int idx, List<Integer> comb, int total, List<List<Integer>> res) {
-        if (total == target) {
-            res.add(new ArrayList<>(comb));
-            return;
-        }
-        if (total > target || idx >= candidates.length) {
-            return;
-        }
-        comb.add(candidates[idx]);
-        makeCombination(candidates, target, idx, comb, total + candidates[idx], res);
-        comb.remove(comb.size() - 1);
-        makeCombination(candidates, target, idx + 1, comb, total, res);
-    }    
 }
