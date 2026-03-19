6aff221230943eeecc2350ceaa9cbb307bf94299
Aniket Warule <78209263+Aniketwarule@users.noreply.github.com>
2026-03-20 02:04:52 +0530

    Time: 1 ms (99.85%), Space: 94.4 MB (81.75%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,12 +1,13 @@
 class Solution {
     public int maxProfit(int[] nums) {
-        int max =0;
-        int min =nums[0];
+        
+        int maxi=0;
+        int mini=Integer.MAX_VALUE;
         for(int i=0;i<nums.length;i++)
         {
-            min = Math.min(min,nums[i]);
-            max = Math.max(max,nums[i]-min);
+            maxi = Math.max(maxi,nums[i]-mini);
+            mini = Math.min(mini,nums[i]);
         }
-        return max;
+        return maxi;
     }
 }
